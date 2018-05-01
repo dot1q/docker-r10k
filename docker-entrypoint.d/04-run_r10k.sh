b469248deaeb917f8244b265d8a9e8b3c7893dd6
Ubuntu <ubuntu@ip-10-0-0-12.us-west-2.compute.internal>
2018-05-01 23:31:01 +0000

    Added error codes for script failures to use in jenkin jobs

--- docker-entrypoint.d/04-run_r10k.sh
+++ docker-entrypoint.d/04-run_r10k.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 r10k deploy environment -pv
-# Sleep to allow filesystem to sync
-sleep 10
+if [ $? -ne 0 ]; then
+  exit 1
+else
+  # Sleep to allow filesystem to sync
+  sleep 10
+fi
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ DIR=/docker-entrypoint.d
 
 if [[ -d "$DIR" ]]
 then
-  /bin/run-parts --verbose --regex '\.(sh|rb)$' "$DIR"
+  /bin/run-parts --verbose --regex '\.(sh|rb)$' "$DIR" || exit 1
 fi
 
 exec "$@"
